ace09aa579bdc922de2925decef020cc93d47f8f
Greg <gkjono@users.noreply.github.com>
2017-07-14 12:14:34 -0400

    Remove-InactiveDevicesFromSCCM

---
+++ TervisSystemCenterConfigurationManager.psm1
@@ -0,0 +1,36 @@
+﻿function Remove-InactiveDevicesFromSCCM {
+    $sitecode = (get-WMIObject -Namespace "root\SMS" -Class "SMS_ProviderLocation" | Select -ExpandProperty SiteCode) + ':'
+    $installdrive = "C:"
+    $localdomain = Get-ADDomain | Select -ExpandProperty DNSRoot
+    $maxdevices = 3000
+
+    IF(test-path ($installdrive + "\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin")) {
+        Import-Module ($installdrive + "\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1")
+    }
+    ELSE
+    {IF(test-path ($installdrive + "\Program Files\Microsoft Configuration Manager\AdminConsole\bin")){
+        Import-Module ($installdrive + "\Program Files\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1")
+        }
+        ELSE
+        {
+        Write-Log -Message "ConfigMgr 2012 R2 Admin console not found!" -severity 1 -component "Error"
+        }
+    }
+
+    Set-Location $sitecode
+    Set-CMQueryResultMaximum -Maximum $maxdevices
+    $SCCMComputers = Get-CMDevice -CollectionName "All Systems"
+
+    Foreach ($SCCMComputer in $SCCMComputers) {
+        $ADComputer = Get-TervisADComputer ($SCCMComputer).Name -Properties lastlogontimestamp,created 
+        If ($ADComputer -and (($ADComputer).TervisLastLogon -lt (Get-Date).AddDays(-30)) -and (($ADComputer).Created -lt (Get-Date).AddDays(-30)) -and (($SCCMComputer).IsActive -eq $false)) {
+            ($SCCMComputer).Name + " is inactive"
+            Remove-CMDevice $SCCMComputer -Force -Confirm:$false
+        } elseif (-NOT ($ADComputer)) {
+            if (($SCCMComputer).IsActive -eq $false) {
+                ($SCCMComputer).Name + " is no longer in AD"
+                Remove-CMDevice $SCCMComputer -Force -Confirm:$false
+            }
+        }
+    }
+}

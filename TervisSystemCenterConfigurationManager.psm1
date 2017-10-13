aa87e761e3103290bc1ab2093dba012a2fb512ab
Greg <gkjono@users.noreply.github.com>
2017-10-13 14:25:34 -0400

    + Add-SCCMDataDrive

--- TervisSystemCenterConfigurationManager.psm1
+++ TervisSystemCenterConfigurationManager.psm1
@@ -37,10 +37,10 @@
 
 function Invoke-SCCM2016Provision {
     Invoke-ApplicationProvision -ApplicationName "SCCM2016" -EnvironmentName Infrastructure
-    #$Nodes = Get-TervisApplicationNode -ApplicationName "SCCM 2016" -EnvironmentName $EnvironmentName
+    #$Nodes = Get-TervisApplicationNode -ApplicationName "SCCM 2016" -EnvironmentName Infrastructure
+    $Nodes = Get-TervisApplicationNode -ApplicationName SCCM2016 -EnvironmentName Infrastructure
+    $Nodes | Add-SCCMDataDrive
     $Nodes | Invoke-SCCMSQLServer2016Install
-    $Nodes | Set-SQLTCPEnabled -InstanceName MSSQLSERVER -Architecture x64
-    $Nodes | Set-SQLTCPIPAllTcpPort -InstanceName MSSQLSERVER -Architecture x64
     $Nodes | New-SQLNetFirewallRule
 }
 
@@ -55,7 +55,16 @@ function Invoke-SCCMSQLServer2016Install {
         $ApplicationDefinition = Get-TervisApplicationDefinition -Name $ApplicationName
         $SQLSACredentials = Get-PasswordstateCredential -PasswordID ($ApplicationDefinition.Environments).SQLSAPassword -AsPlainText
         $SCCMServiceAccountCredentials = Get-PasswordstateCredential -PasswordID ($ApplicationDefinition.Environments).SCCMServiceAccountPassword -AsPlainText
-	    $ChocolateyPackageParameters = "/SAPWD=$($SQLSACredentials.Password) /AGTSVCACCOUNT=$($SCCMServiceAccountCredentials.Username) /AGTSVCPASSWORD=$($SCCMServiceAccountCredentials.Password) /SQLSVCACCOUNT=$($SCCMServiceAccountCredentials.Username) /SQLSVCPASSWORD=$($SCCMServiceAccountCredentials.Password) /RSSVCACCOUNT=$($SCCMServiceAccountCredentials.Username) /RSSVCPASSWORD=$($SCCMServiceAccountCredentials.Password)"
+        Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+            If (-NOT (Test-Path "D:\Databases")) {
+                New-Item -Path "D:\Databases" -ItemType directory
+            }
+            $ACL = Get-Acl "D:\Databases"
+            $AcessRule = New-Object  system.security.accesscontrol.filesystemaccessrule(($Using:SCCMServiceAccountCredentials).UserName,"FullControl”,”ContainerInherit,ObjectInherit”,”None”,”Allow”)
+            $ACL.SetAccessRule($AcessRule)
+            Set-Acl "D:\Databases" $ACL
+        }
+	    $ChocolateyPackageParameters = "/SAPWD=$($SQLSACredentials.Password) /AGTSVCACCOUNT=$($SCCMServiceAccountCredentials.Username) /AGTSVCPASSWORD=$($SCCMServiceAccountCredentials.Password) /SQLSVCACCOUNT=$($SCCMServiceAccountCredentials.Username) /SQLSVCPASSWORD=$($SCCMServiceAccountCredentials.Password) /RSSVCACCOUNT=$($SCCMServiceAccountCredentials.Username) /RSSVCPASSWORD=$($SCCMServiceAccountCredentials.Password) /SQLTEMPDBDIR=D:\Databases /SQLTEMPDBLOGDIR=D:\Databases /SQLUSERDBDIR=D:\Databases /SQLUSERDBLOGDIR=D:\Databases"
         $ChocolateyPackage = '\\' + $DNSRoot + '\Applications\Chocolatey\SQLServer2016Standard.2016.1702.0.nupkg'
 
     }
@@ -63,5 +72,28 @@ function Invoke-SCCMSQLServer2016Install {
 	    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
 		    choco install SQLServer2016Standard -y -s $Using:ChocolateyPackage --package-parameters=$($using:ChocolateyPackageParameters)
 	    }
+    }
+}
+
+function Add-SCCMDataDrive {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]$ComputerName,
+        [Parameter(Mandatory,ValueFromPipeline)]$IPAddress
+    )
+    $VM = Find-TervisVMByIP $IPAddress
+    $VMHardDiskDrives = Get-VMHardDiskDrive -ComputerName ($VM).ComputerName -VMName $ComputerName
+    if (-NOT (($VMHardDiskDrives).Count -ge 2)) {
+        $StoragePath = (Split-Path -Path ($VMHardDiskDrives).Path) + '\' + $ComputerName + '-D.vhdx'
+        $CimSession = New-CimSession -ComputerName $ComputerName
+        if (-NOT (Test-Path -Path $StoragePath)) {
+            New-VHD -Path $StoragePath -Dynamic -SizeBytes 500GB -ComputerName ($VM).ComputerName 
+        }
+        Get-VMScsiController -ComputerName ($VM).ComputerName -VMName $ComputerName -ControllerNumber 0 |
+            Add-VMHardDiskDrive -ComputerName ($VM).ComputerName -VMName $ComputerName -Path $StoragePath -ControllerType SCSI -ControllerNumber 0
+        Get-Disk -CimSession $CimSession | 
+            Where {$_.NumberOfPartitions -eq 0 -and $_.PartitionStyle -eq "RAW" -and $_.Size -eq 536870912000} |
+            Initialize-Disk -Passthru |
+            New-Partition -AssignDriveLetter -UseMaximumSize |
+            Format-Volume -FileSystem NTFS -Confirm:$false -Force
     }
 }
